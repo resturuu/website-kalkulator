1af54b2a66cf2ec7185992b6023ed5c63ff60b78
Restu Ardiansyah <135223400+resturuu@users.noreply.github.com>
2023-07-23 18:27:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendValue(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
